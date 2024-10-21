d58ed52c4acdaa2f83c560d57b5b20e36cf7acd5
abstractionjackson <47265694+abstractionjackson@users.noreply.github.com>
2024-10-21 13:56:17 -0700

    update workflow and config

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,30 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig(({ mode }) => {
-  const config = {
-    base: "/pale-nimbus",
-    build: {
-      lib: {
-        entry: resolve(__dirname, "src/lib/index.js"),
-        name: "Pale Nimbus",
-        fileName: "pale-nimbus",
-        formats: ["es", "umd"],
-      },
-    },
-  };
-  if (mode === "development" || mode === "demo") {
-    config.root = "demo";
-  }
   if (mode === "demo") {
-    config.build.outDir = "../docs";
-    config.emptyOutDir = true;
-    config.server = {
-      port: 3000,
+    return {
+      root: "demo", // demo site as the root during build
+      build: {
+        outDir: "../docs", // Output the demo site to 'docs' for GitHub Pages
+        emptyOutDir: true, // Clean the output directory before building
+      },
     };
   }
-  return config;
+
+  return {
+    build: {
+      lib: {
+        entry: resolve(__dirname, "src/lib/BusinessCard/index.js"),
+        name: "BusinessCardLibrary",
+        fileName: "pale-nimbus",
+        formats: ["es", "umd"], // Output in both ES and UMD formats
+      },
+      rollupOptions: {
+        // Any Rollup options you need (e.g., external libraries)
+      },
+      outDir: "dist", // Output library build to 'dist'
+      emptyOutDir: true, // Clean the 'dist' folder before building
+    },
+  };
 });
